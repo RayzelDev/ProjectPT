12ff4a6c2522b2bcafee37b4a053a6b6dc5f4c8c
William Varvenczak <willian.varvenczak@linedata.com.br>
2025-02-05 14:46:20 -0300

    adicionado HINT nas lanças, garras e arcos do 80d e 100a

--- SrcGame/src/Game/Engine/UI/UIItemInfoBox.cpp
+++ SrcGame/src/Game/Engine/UI/UIItemInfoBox.cpp
@@ -788,6 +788,30 @@ namespace UI
 			AddString(0, "Para Craftar A Foice da Morte\nUtilize 3 Enigmas\n ", D3DCOLOR_ARGB(240, 215, 244, 238));
 			AddString(1, "");
 			break;
+		case (sinWT1 | sin20):
+			AddString(0, "Para Craftar A Lança Imortal\nUtilize 3 Infernas\n ", D3DCOLOR_ARGB(240, 215, 244, 238));
+			AddString(1, "");
+			break;
+		case (sinWT1 | sin22):
+			AddString(0, "Para Craftar A Lança Herege\nUtilize 3 Enigmas\n ", D3DCOLOR_ARGB(240, 215, 244, 238));
+			AddString(1, "");
+			break;
+		case (sinWC1 | sin19):
+			AddString(0, "Para Craftar A Garra Vibora\nUtilize 3 Infernas\n ", D3DCOLOR_ARGB(240, 215, 244, 238));
+			AddString(1, "");
+			break;
+		case (sinWC1 | sin21):
+			AddString(0, "Para Craftar A Garra da Injustiça\nUtilize 3 Enigmas\n ", D3DCOLOR_ARGB(240, 215, 244, 238));
+			AddString(1, "");
+			break;
+		case (sinWS1 | sin21):
+			AddString(0, "Para Craftar O Arco Wyvern\nUtilize 3 Infernas\n ", D3DCOLOR_ARGB(240, 215, 244, 238));
+			AddString(1, "");
+			break;
+		case (sinWS1 | sin23):
+			AddString(0, "Para Craftar O Arco Imortal\nUtilize 3 Enigmas\n ", D3DCOLOR_ARGB(240, 215, 244, 238));
+			AddString(1, "");
+			break;
 		case (sinBI1 | sin10):
 			AddString(0, "Diminui a chance de Falha do Aging em 20%!", D3DCOLOR_ARGB(255, 245, 240, 218));
 			AddString(1, "");
