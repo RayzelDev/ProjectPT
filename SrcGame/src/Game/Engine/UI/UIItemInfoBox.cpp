d09aa9f72f281dff906d9f2ab32a9232a6117da2
William Cardoso Varvenczak <williamcardosowarwenczack@gmail.com>
2025-02-05 20:09:02 -0300

    adicionado craft bora 80d-100a-102

--- SrcGame/src/Game/Engine/UI/UIItemInfoBox.cpp
+++ SrcGame/src/Game/Engine/UI/UIItemInfoBox.cpp
@@ -860,6 +860,14 @@ namespace UI
 			AddString(0, "Para Craftar A Luva Fenix\nUtilize 3 Enigmas\n ", D3DCOLOR_ARGB(255, 132, 50, 254));
 			AddString(1, "");
 			break;
+		case (sinDB1 | sin19):
+			AddString(0, "Para Craftar A Bota Inferno\nUtilize 3 Infernas\n ", D3DCOLOR_ARGB(255, 132, 50, 254));
+			AddString(1, "");
+			break;
+		case (sinDB1 | sin21):
+			AddString(0, "Para Craftar A Bota Fenix\nUtilize 3 Enigmas\n ", D3DCOLOR_ARGB(255, 132, 50, 254));
+			AddString(1, "");
+			break;
 		case (sinBI1 | sin10):
 			AddString(0, "Diminui a chance de Falha do Aging em 20%!", D3DCOLOR_ARGB(255, 245, 240, 218));
 			AddString(1, "");
