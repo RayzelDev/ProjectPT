863ac322016ee6325aec7acdac6193316078c768
William Cardoso Varvenczak <williamcardosowarwenczack@gmail.com>
2025-02-05 21:02:25 -0300

    adicionado orbital no craft 80-100-102

--- SrcGame/src/Game/Engine/UI/UIItemInfoBox.cpp
+++ SrcGame/src/Game/Engine/UI/UIItemInfoBox.cpp
@@ -892,6 +892,14 @@ namespace UI
 			AddString(0, "Para Craftar O Amuleto Mistico+3\nUtilize 3 Enigmas\n ", D3DCOLOR_ARGB(255, 132, 50, 254));
 			AddString(1, "");
 			break;
+		case (sinOM1 | sin20):
+			AddString(0, "Para Craftar O Orbital Clairvoyance\nUtilize 3 Infernas\n ", D3DCOLOR_ARGB(255, 132, 50, 254));
+			AddString(1, "");
+			break;
+		case (sinOM1 | sin22):
+			AddString(0, "Para Craftar O Orbital Arvenus\nUtilize 3 Enigmas\n ", D3DCOLOR_ARGB(255, 132, 50, 254));
+			AddString(1, "");
+			break;
 		case (sinBI1 | sin10):
 			AddString(0, "Diminui a chance de Falha do Aging em 20%!", D3DCOLOR_ARGB(255, 245, 240, 218));
 			AddString(1, "");
